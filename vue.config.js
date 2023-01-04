9f947ac244edfec0412e05b6153ef0ef892b45c6
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-01-05 02:22:01 +0300

    Icons added

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  pwa: {
+    name: "Lists",
+    short_name: "Lists",
+    display: "standalone",
+    themeColor: "#f6ca09",
+    background_color: "#000000"
+  },
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
