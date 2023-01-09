8dda70ca81604cedef2a7dea225108955a865c89
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-01-09 17:21:44 +0300

    deploy

---
+++ js/307.7f02e11a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunklists"]=self["webpackChunklists"]||[]).push([[307],{8307:function(n,s,t){t.r(s),t.d(s,{default:function(){return f}});var u=t(3396);const a={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),r=[e];function i(n,s){return(0,u.wg)(),(0,u.iD)("div",a,r)}var c=t(89);const l={},o=(0,c.Z)(l,[["render",i]]);var f=o}}]);
+//# sourceMappingURL=307.7f02e11a.js.map
